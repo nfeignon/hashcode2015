42e327923fc21920a7bd5ae6adca0a124d88193c
Nicolas Feignon <nfeignon@gmail.com>
2015-03-12 19:45:32 +0100

    capa mini

--- main.py
+++ main.py
@@ -13,16 +13,40 @@ class Serveur:
         self.x = 0
         self.y = 0
 
+def get_capacite_garantie(groupes):
+    capa_mini = int(float(inf))
+    for group in groupes:
+
+        capa = 0
+        for s in group:
+            capa += s.capacite
+
+        for r in rangees:
+            for i in range(r):
+                if isinstance(r[i], Serveur):
+                    capa -= r[i].capacite
+
+                    i += r[i].emplacements - 1
+
+            if capa_mini > capa:
+                capa_mini = capa
+
+    return capa_mini
+
+
+
 
 if __name__ == '__main__':
     print 'Hash code\n\n'
 
     n_rangees, emplacements, n_indisponibles, n_groupes, n_serveurs = [int(i) for i in raw_input().split()]
     print str(n_groupes) + ' groupes'
+    print str(n_serveurs) + ' serveurs'
 
     indisponibles = []
     serveurs = []
     rangees = []
+    groupes = []
 
     for i in range(n_rangees):
         rangees.append(['.' for j in range(emplacements)])
