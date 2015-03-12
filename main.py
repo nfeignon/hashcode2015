846915838953c727f661d7dcdf7810422c055b51
Nicolas Feignon <nfeignon@gmail.com>
2015-03-12 19:04:08 +0100

    parse entry

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ import os
 
 if __name__ == '__main__':
     print 'Hash code'
+
+    rangees, emplacements, n_indisponibles, groupes, n_serveurs = [int(i) for i in raw_input().split()]
+
+    indisponibles = []
+    serveurs = []
+
+    for i in range(n_indisponibles):
+        indisponibles.append([int(j) for j in raw_input().split()])
+
+    for i in range(n_serveurs):
+        serveurs.append([int(j) for j in raw_input().split()])
+
+    print indisponibles
+    print serveurs
