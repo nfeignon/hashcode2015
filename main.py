ca45ba391653ab5c93878e5e4f73933750306ef7
Nicolas Feignon <nfeignon@gmail.com>
2015-03-27 21:20:50 +0100

    wip

--- main.py
+++ main.py
@@ -40,12 +40,13 @@ def partager_pizza(R,C,H,S):
                     R2 = i + x
                     C2 = j + y
 
-                    size = (R2-R1+1)*(C2-C1+1)
 
+                    size = (R2-R1+1)*(C2-C1+1)
                     if size >= 3 and size <= 12:
-                        if tester_part(R1, C1, R2, C2):
-                            parts.append([R1, C1, R2, C2])
-                            write_part(R1, C1, R2, C2)
+                        if R2 < R and C2 < C:
+                            if tester_part(R1, C1, R2, C2):
+                                parts.append([R1, C1, R2, C2])
+                                write_part(R1, C1, R2, C2)
 
 
 
