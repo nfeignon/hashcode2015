5c695a61b1f5ef1bd2fc061f178783e41a56a859
Nicolas Feignon <nfeignon@gmail.com>
2015-03-27 21:40:43 +0100

    wip

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def tester_part(R1, C1, R2, C2):
 
     for i in range(R1, R2+1):
         for j in range(C1, C2+1):
+            if pizza[i][j] == 'X':
+                return False
+
             if pizza[i][j] == 'H':
                 jambons += 1
 
@@ -30,6 +33,7 @@ def partager_pizza(R,C,H,S):
             R1 = i
             C1 = j
 
+            parts2 = []
             for x in range(4):
                 for y in range(4):
                     R2 = i + x
@@ -40,12 +44,21 @@ def partager_pizza(R,C,H,S):
                     if size >= 3 and size <= 12:
                         if R2 < R and C2 < C:
                             if tester_part(R1, C1, R2, C2):
-                                parts.append([R1, C1, R2, C2])
+                                parts2.append([R1, C1, R2, C2])
                                 write_part(R1, C1, R2, C2)
 
+            size_min = 20
+            part_min = []
 
+            print parts2
+            for part in parts2:
+                size = (part[2]-part[0]+1)*(part[3]-part[1]+1)
+                if size < size_min:
+                    size_min = size
+                    part_min = part
 
-
+            if len(parts2) > 0:
+                parts.append(part_min)
 
     return parts
 
