5c8efc9b2030e47c8940ec7d41fc49ebaac80b09
Vincent Timofti <vincent.timofti@gmail.com>
2015-03-28 11:56:51 +0100

    affichage map

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import os
 
 
 
+def print_map(map):
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            sys.stdout.write (map[i][j])
+        print 
+
 print "Hash code final round"
 
 ROWS,COLUMNS,ALTITUDES = [int(i) for i in raw_input().split()]
@@ -16,20 +22,37 @@ print "CIBLES:%s, RAYON:%s, BALLONS:%s, TOURS:%s" % (CIBLES, RAYON, BALLONS, TOU
 print "ROW DEPART:%s, COLUMN DEPART:%s" % (R_DEPART, C_DEPART)
 
 cibles = []
+cibles_dict = {}
+
+for i in range(ROWS):
+    for j in range(COLUMNS):
+        cibles_dict[(i, j)] = False
 
 for i in range(CIBLES):
-    cibles.append([int(i) for i in raw_input().split()])
+    cibles.append([int(j) for j in raw_input().split()])
+    cibles_dict[(cibles[i][0], cibles[i][1])] = True
 
 altitudes = []
 
 for i in range(ALTITUDES):
     altitude = []
     for j in range(ROWS):
-        data = [int(i) for i in raw_input().split()]
+        data = [int(x) for x in raw_input().split()]
         altitude.append(zip(data[0::2], data[1::2]))
     altitudes.append(altitude)
 
-print altitudes[0][0]
+carte_cibles = []
+
+for i in range(ROWS):
+    row = []
+    for j in range(COLUMNS):
+        if cibles_dict[(i, j)] == True:
+            row.append('X')
+        else:
+            row.append('.')
+    carte_cibles.append(row)
+
+print_map(carte_cibles)
 
 
 
